c3f44f2cdaac77c5b896418e1f368bc30d7cf905
Madhur <madhurmoms@gmail.com>
2023-05-14 00:36:50 +0530

    done with jumbotron

--- src/MyComponents/Home.js
+++ src/MyComponents/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Jumbotron, Button } from 'bootstrap-4-react';
+import { Container } from "react-bootstrap";
 
 const Home = () => {
     return (
         <div>
-            <Jumbotron className = "text-center">
-                <h1>JAVA</h1>
+            <Jumbotron className="text-center">
+                <h1 className="display-3">JAVA</h1>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, culpa eum rerum atque accusantium neque cumque repellendus quibusdam tenetur, repudiandae corrupti maxime incidunt quam deleniti obcaecati pariatur? Aut, facilis? Libero.</p>
-            
+                <Container>
+                    <Button color="primary" outline >Start Using</Button>
+                </Container >
             </Jumbotron >
         </div>
     )
