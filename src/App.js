3b04e5ef5d0601eb74e1dfcbc64e9125fe8adfdb
madhurmoms <madhurmoms@gmail.com>
2023-05-13 21:36:35 +0530

    course app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+   <div>
+    <h1>This is React Bootstrap</h1>
+    <button>First React</button>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Footer className='bg-dark text-light py-3'>
+      <p className="text-center"> Copyright &copy; MyTodosList.com </p>
+    </Footer>
+  )
+}
+
+export default Footer
