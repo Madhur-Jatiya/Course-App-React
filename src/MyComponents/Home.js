9863de609a5a70e270efcc8b5923c86af6ec74e9
Madhur <madhurmoms@gmail.com>
2023-05-14 00:33:04 +0530

    jumbotron

--- src/MyComponents/Home.js
+++ src/MyComponents/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Button } from 'bootstrap-4-react';
 
 const Home = () => {
     return (
         <div>
-            <h1>JAVA</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, culpa eum rerum atque accusantium neque cumque repellendus quibusdam tenetur, repudiandae corrupti maxime incidunt quam deleniti obcaecati pariatur? Aut, facilis? Libero.</p>
+            <Jumbotron className = "text-center">
+                <h1>JAVA</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, culpa eum rerum atque accusantium neque cumque repellendus quibusdam tenetur, repudiandae corrupti maxime incidunt quam deleniti obcaecati pariatur? Aut, facilis? Libero.</p>
+            
+            </Jumbotron >
         </div>
     )
 }
