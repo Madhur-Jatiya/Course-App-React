8f554a041f909f8a9a66f382ac4f08ed248fdbe9
Madhur <madhurmoms@gmail.com>
2023-05-14 00:23:01 +0530

    header and home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import Header from './MyComponents/Header';
+import Home from './MyComponents/Home';
+// import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+  // const btnHandle = () => {
+  //   toast("This is my first message");
+  //   toast.success("This is my success message");
+  //   toast.error("This is my error message");
+  //   toast.warning("This is my error message");
+  // }
   return (
-   <div>
-    <h1>This is React Bootstrap</h1>
-    <button>First React</button>
-   </div>
+    <div className='text-center'>
+      {/* <ToastContainer /> */}
+      <Header />
+      <Home/>
+    </div>
   );
 }
 
---
+++ src/MyComponents/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Jumbotron } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>JAVA</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, culpa eum rerum atque accusantium neque cumque repellendus quibusdam tenetur, repudiandae corrupti maxime incidunt quam deleniti obcaecati pariatur? Aut, facilis? Libero.</p>
+        </div>
+    )
+}
+export default Home;
