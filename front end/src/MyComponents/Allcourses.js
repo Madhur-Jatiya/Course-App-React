fcad9d2834cef90533deec9082ad5f71faac8168
madhurmoms <madhurmoms@gmail.com>
2023-05-16 01:36:34 +0530

    front end and backend work properly

--- front end/src/MyComponents/Allcourses.js
+++ front end/src/MyComponents/Allcourses.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Course from "./Course";
+import myAxios from ".//../api/bootapi";
 
 const Allcourses = () => {
   useEffect(() => {
     document.title = "All Courses || Madhurmoms";
-  });
+    getAllCoursesFromServer();
+  }, []);
 
-  const [courses, setCourses] = useState([
-    { id: 1, title: "React", description: "Front End framework" },
-    { id: 2, title: "Java", description: "Back End framework" },
-    { id: 3, title: "Angular", description: "Front End framework" },
-    { id: 4, title: "Vue", description: "Front End framework" },
-    { id: 5, title: "Node", description: "Back End framework" },
-    { id: 6, title: "Express", description: "Back End framework" },
-    { id: 7, title: "MongoDB", description: "Database" },
-  ]);
+  const getAllCoursesFromServer = () => {
+    myAxios.get('/courses').then(
+      (response) => {
+        console.log(response.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  const [courses, setCourses] = useState([]);
 
   return (
     <div>
